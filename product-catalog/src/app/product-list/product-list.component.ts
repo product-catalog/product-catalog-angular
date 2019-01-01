9498aff341852113fdc825dad28737a025a44ce9
siqqQ <b.bohosyan@gmail.com>
2019-01-01 15:56:21 +0200

    Searching by price is added

--- product-catalog/src/app/product-list/product-list.component.ts
+++ product-catalog/src/app/product-list/product-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../shared/product/product.service';
 import { Router } from '@angular/router';
+import { Product } from '../dtos';
+import { isRegExp } from 'util';
 
 @Component({
   selector: 'app-product-list',
@@ -10,12 +12,41 @@ import { Router } from '@angular/router';
 })
 export class ProductListComponent implements OnInit {
   products: Array<any>;
+  filteredProducts: Array<any>;
+  filterPriceValue: any;
+  filterNameValue: string;
   constructor(private productService: ProductService, private _router: Router) { }
 
   ngOnInit() {
+    this.filterPriceValue = 0;
+    this.filterNameValue = "";
     this.productService.getAll().subscribe(
       data => {
         this.products = data;
+        this.filteredProducts = data;
+      },
+      error => console.error(error)
+    );
+  }
+
+  changeNameInput(event){
+    this.filterNameValue = event.target.value;
+    this.productService.getAll().subscribe(
+      data => {
+        this.filteredProducts = this.products;
+        this.filteredProducts = this.filteredProducts.filter(product => new RegExp(this.filterNameValue).test(product.name)).filter(product => product.price > this.filterPriceValue);
+      },
+      error => console.error(error)
+    );
+  }
+
+  changePrice(event){
+    this.filterPriceValue = event.target.value;
+    this.productService.getAll().subscribe(
+      data => {
+        this.filteredProducts = this.products;
+        this.filteredProducts = this.filteredProducts.filter(product => new RegExp(this.filterNameValue).test(product.name));
+        this.filteredProducts = this.filteredProducts.filter(product => product.price > this.filterPriceValue);
       },
       error => console.error(error)
     );
